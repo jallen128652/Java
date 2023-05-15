8e79979085333dd112a2101d88d069105dfa91dd
jallen128652 <jallen128652@mymail.tstc.edu>
2023-05-15 17:18:43 -0500

    Practice

--- Practice/src/First.java
+++ Practice/src/First.java
@@ -1,8 +1,14 @@
+// comments
+/* block comments */
+//class name
 public class First
 {
+// main method aka function
     public static void main(String[] args)
     {
+// code
         System.out.println("My new and improved");
         System.out.println("Java application");
+        System.out.println("Look at the .class file after debugging to see if it re compiles");
     }
 }
---
+++ Practice/src/FirstDialog.java
@@ -0,0 +1,14 @@
+//First GUI class
+
+//required library
+import javax.swing.JOptionPane;
+// class name
+public class FirstDialog
+{
+//  main method
+    public static void main(String[] args)
+    {
+// popup box with message
+        JOptionPane.showMessageDialog(null, "First Java dialog");
+    }
+}
