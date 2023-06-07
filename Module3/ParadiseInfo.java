434b860b372cb95afaac88399952b862de12084b
jallen128652 <jallen128652@mymail.tstc.edu>
2023-06-07 11:09:21 -0500

    methods

---
+++ Module3/ParadiseInfo.java
@@ -0,0 +1,13 @@
+public class ParadiseInfo {
+    public static void main(String[] args)
+    {
+//        function call inside main
+        displayInfo();
+    }
+//    function
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}
---
+++ Module3/TestInfo.java
@@ -0,0 +1,8 @@
+public class TestInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("Calling method from other class:");
+//        how to call the method from another class
+        ParadiseInfo.displayInfo();
+    }
+}
