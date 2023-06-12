6a7ef1a35a9b1c8824f5deb5373138db8819b2a0
jallen128652 <jallen128652@mymail.tstc.edu>
2023-06-12 17:34:33 -0500

    methods

--- Module3/CreateSpaServices.java
+++ Module3/CreateSpaServices.java
@@ -2,39 +2,12 @@
 import java.util.Scanner;
 public class CreateSpaServices {
     public static void main(String[] args) {
-//        declare variables
-//        String service;
-//        double price;
-
 //        declare objects
         SpaService firstService = new SpaService();
         SpaService secondService = new SpaService();
-//        Scanner keyboard = new Scanner(System.in);
 //        new methods for get to replace the inputs
         firstService = getData(firstService);
         secondService = getData(secondService);
-////        get input from the user
-//        System.out.print("Enter service: ");
-//        service = keyboard.nextLine();
-//        System.out.print("Enter price: ");
-//        price = keyboard.nextDouble();
-//
-////        call set method in spaService non-static class
-//        firstService.setServiceDescription(service);
-//        firstService.setPrice(price);
-
-//        clear input buffer
-//        keyboard.nextLine();
-
-//        get inputs for secondService
-//        System.out.print("Enter service: ");
-//        service = keyboard.nextLine();
-//        System.out.print("Enter price: ");
-//        price = keyboard.nextDouble();
-//
-////        call set method in spaService non-static class
-//        secondService.setServiceDescription(service);
-//        secondService.setPrice(price);
 
 
 
