ec3af046e7b5b3cf96423313621dc058df707a84
jallen128652 <jallen128652@mymail.tstc.edu>
2023-06-22 11:43:10 -0500

    Module 4 practice

--- Module4/Practice/src/AssignVolunteer.java
+++ Module4/Practice/src/AssignVolunteer.java
@@ -7,18 +7,24 @@ public class AssignVolunteer {
         final int OTHER_CODE = 2;
         final String CLOTHING_PRICER = "Regina";
         final String OTHER_PRICER = "Marco";
+        String message;
 
         Scanner input = new Scanner(System.in);
         System.out.println("What type of donation is this?");
         System.out.print("Enter " + CLOTHING_CODE + " for clothing, " + OTHER_CODE + " for anything else: ");
         donationType = input.nextInt();
 
-        if(donationType == CLOTHING_CODE)
+        if(donationType == CLOTHING_CODE) {
             volunteer = CLOTHING_PRICER;
-        else
+            message = "a clothing donation.";
+        }
+        else {
             volunteer = OTHER_PRICER;
+            message = "a non-clothing item.";
+        }
 
         System.out.println("You entered " + donationType);
         System.out.println("The volunteer who will price this item is " + volunteer);
+        System.out.println("This is " + message);
     }
 }
