b03d13a684c81008bb8d132252782eee7ed52255
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-03 17:52:28 -0500

    loop practice

--- Module5/Practice/src/PreAndPostCrementDemo.java
+++ Module5/Practice/src/PreAndPostCrementDemo.java
@@ -26,5 +26,11 @@ public class PreAndPostCrementDemo {
         System.out.println("v is " + v);
         System.out.println("++v is " + plusPlusV);
         System.out.println("v++ is " + vPlusPlus);
+//        third example
+        int w = 17, x = 17, y = 18;
+        boolean compare1 = (++w == y);
+        boolean compare2 = (x++ == y);
+        System.out.println("First compare is " + compare1);
+        System.out.println("Second compare is " + compare2);
     }
 }
