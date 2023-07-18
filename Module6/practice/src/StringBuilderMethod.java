bf59b66426cdf620ce55c7d469594be3012ec319
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-18 15:37:53 -0500

    mod 6 practice

---
+++ Module6/practice/src/StringBuilderMethod.java
@@ -0,0 +1,15 @@
+public class StringBuilderMethod {
+    public static void main(String[] args){
+//        class object = new classConstructor("StringText");
+        StringBuilder str = new StringBuilder("singing");
+        System.out.println(str);
+        str.append(" in the dead of ");
+        System.out.println(str);
+        str.insert(0, "Black");
+        System.out.println(str);
+        str.insert(5, "bird ");
+        System.out.println(str);
+        str.append("night");
+        System.out.println(str);
+    }
+}
