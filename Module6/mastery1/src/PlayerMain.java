2fb0602a12269661e14117a8e628a57e2aaa8e1c
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-20 18:08:12 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	deleted:    Module6/mastery/.idea/.gitignore
    	deleted:    Module6/mastery/.idea/misc.xml
    	deleted:    Module6/mastery/.idea/modules.xml
    	deleted:    Module6/mastery/.idea/vcs.xml
    	deleted:    Module6/mastery/Instructions.txt
    	deleted:    Module6/mastery/Player.java
    	deleted:    Module6/mastery/PlayerMain.java
    	deleted:    Module6/mastery/mastery.iml

--- Module6/mastery1/src/PlayerMain.java
+++ Module6/mastery1/src/PlayerMain.java
@@ -44,7 +44,7 @@ public class PlayerMain {
 
     }
 
-    // gameFx: create function for comparing the stringbuilder, append, prompt,
+    // gameFx: function for comparing the stringbuilder, append, prompt,
     // search, append, tostring, compare, print, prompt again
     public static void gameFx(StringBuilder phrase1, Player player1) {
         // store original phrase as a string
@@ -55,9 +55,13 @@ public class PlayerMain {
         String clueString;
         // declare var for the to pass into the Player class numberAttempts()
         int numAttempts = 0;
+
         // must also be converted tostring each round and stored in a separate var name
         // loop through the length and store the @ value in clue for ea char in phrase1
         do {
+            // clear the stringbuilder each round
+            clue.setLength(0);
+
             int length = phrase1.length();
             for (int x = 0; x < length; x++) {
                 if (phrase1.charAt(x) != ' ') {
@@ -72,27 +76,51 @@ public class PlayerMain {
             System.out.println("Please guess a letter and press enter or enter a complete guess of the phrase. ");
             // input
             Scanner input2 = new Scanner(System.in);
-            String attempt = input2.nextLine();
+            // convert toLower so its case insensitive
+            String attempt = input2.nextLine().toLowerCase();
             // get input length to determine if it's a char or a complete guess
             int attLength = attempt.length();
             // call to number of attempts in player class, will increment the value
-            player1.numberAttempts(numAttempts);
-            if (attLength == length) {
-                if (originalPhrase.equals(attempt)) {
+            // declare var for the to pass into the Player class var numAttempts and method
+            // numberAttempts()
+            numAttempts = player1.numberAttempts(numAttempts);
+
+            if (attLength == 1) {
+                // Single-letter guess
+                int index = originalPhrase.indexOf(attempt);
+                while (index != -1) {
+                    clue.setCharAt(index, Character.toLowerCase(originalPhrase.charAt(index))); // Convert to lowercase
+                    // before updating
+                    index = originalPhrase.indexOf(attempt, index + 1);
+                }
+
+                if (!clue.toString().contains("@")) {
+                    // All letters have been revealed
                     System.out.println("Congratulations, you win!");
+                    player1.display(player1.getName(), numAttempts);
+                    return;
+                } else if (originalPhrase.indexOf(attempt) != -1) {
+                    // At least one match found
+                    System.out.println("Correct guess!");
+                } else {
+                    // No matches found
+                    System.out.println("No matches for " + attempt);
+                }
+            } else {
+                // Complete phrase guess
+                if (originalPhrase.equals(attempt)) {
+                    // Display congratulatory message with player name
+                    System.out.println("Congratulations, you win!");
+                    player1.display(player1.getName(), numAttempts);
+                    return;
                 } else {
                     System.out.println("Incorrect, please continue");
                 }
-            } else {
-                // compare input char to phrase using indexOf()
-                // if(match){
-                // append clue
-                // }
             }
             // converts clue and updates the loop control
             clueString = clue.toString();
 
-        } while (!originalPhrase.equals(clueString));
-        // call display and reset numAtt
+        } while (clueString.contains("@"));
+        player1.display(player1.getName(), numAttempts);
     }
 }
