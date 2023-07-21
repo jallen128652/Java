9cd940d675140e7b40f14e6a07bfed75c7bda93d
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-20 23:22:42 -0500

    On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   Module6/practice/src/PlayerMain.java

--- Module6/practice/src/PlayerMain.java
+++ Module6/practice/src/PlayerMain.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class PlayerMain {
     public static void main(String[] args) {
         // create instance object of the player class
@@ -14,7 +15,7 @@ public class PlayerMain {
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
-//        constructor calls
+        // constructor calls
         // note format classObject.classMethod();
         player1.setName(name);
 
@@ -31,41 +32,41 @@ public class PlayerMain {
 
     // randPhraseFx: create functions for storing and calling phrases 5 phrases
     public static void randPhrase(StringBuilder phrase1) {
-//        reset phase1 each round
+        // reset phase1 each round
         phrase1.setLength(0);
-//        generate rand
+        // generate rand
         int rand = (int) (Math.random() * 5);
-//        declare and initialize the String array
+        // declare and initialize the String array
         String[] phrase = new String[5];
         phrase[0] = "welcome to the thunderdome";
         phrase[1] = "have a nice day";
         phrase[2] = "where do you want to eat";
         phrase[3] = "are we there yet";
         phrase[4] = "how are you doing";
-//        stores the phrase in the stringbuilder phrase1
+        // stores the phrase in the stringbuilder phrase1
         phrase1.replace(0, phrase.length - 1, phrase[rand]);
     }
 
-    public static void cluePhrase(StringBuilder phrase1, StringBuilder clue){
-//   reset clue each round
+    public static void cluePhrase(StringBuilder phrase1, StringBuilder clue) {
+        // reset clue each round
         clue.setLength(0);
-//        get length of the phrase
+        // get length of the phrase
         int length = phrase1.length();
-//        loop length times
+        // loop length times
         for (int x = 0; x < length; x++) {
-//            store characters in clue
-//            note I chose replace instead of append because I was having issues after each round clearing clue
-//            likely wasn't clue but phrase1 though.
+            // store characters in clue
+            // note I chose replace instead of append because I was having issues after each
+            // round clearing clue
+            // likely wasn't clue but phrase1 though.
             if (phrase1.charAt(x) != ' ') {
-                clue.replace(x, x,"@");
+                clue.replace(x, x, "@");
             } else {
-                clue.replace(x, x," ");
+                clue.replace(x, x, " ");
             }
         }
     }
 
-    // gameFx: create function for comparing the stringbuilder, append, prompt,
-    // search, append, tostring, compare, print, prompt again
+    // function for comparing the stringbuilder and entering attempts
     public static void gameFx(StringBuilder phrase1, Player player1, StringBuilder clue) {
         // store original phrase as a string
         String originalPhrase = phrase1.toString();
@@ -78,47 +79,49 @@ public class PlayerMain {
             System.out.println("Please guess a letter and press enter or enter a complete guess of the phrase. ");
             // input
             Scanner input2 = new Scanner(System.in);
-//            stores input and converts to lowercase
+            // stores input and converts to lowercase
             String attempt = input2.nextLine().toLowerCase();
             // get input length to determine if it's a char or a complete guess
             int attLength = attempt.length();
             // call to number of attempts in player class, will increment the value
-//            note calls to this method in the Player class don't need an arg ass it's just incrementing
+            // note calls to this method in the Player class don't need an arg ass it's just
+            // incrementing
             player1.numberAttempts();
-//            compares length of attempt with length of phrase to see the type of attempt
+            // compares length of attempt with length of phrase to see the type of attempt
             int length = phrase1.length();
             if (attLength == length) {
-//                determines if the attempt is equal to the phrase
+                // determines if the attempt is equal to the phrase
                 if (attempt.equalsIgnoreCase(phrase1.toString())) {
                     System.out.println("Congratulations, you win!");
                     break;
                 } else {
                     System.out.println("Incorrect, please continue");
                 }
-//                if the user enters just 1 char
+                // if the user enters just 1 char
             } else if (attLength == 1) {
                 char letter = attempt.charAt(0);
                 boolean match = false;
-//                loop through to determine if the chars match
-            for (int i = 0; i < length; i++) {
-                char c = originalPhrase.charAt(i);
-                if (c == letter) {
-//                    replace @ with letter
-                    clue.setCharAt(i, letter);
-                    match = true;
+                // loop through to determine if the chars match
+                for (int i = 0; i < length; i++) {
+                    char c = originalPhrase.charAt(i);
+                    if (c == letter) {
+                        // replace @ with letter
+                        clue.setCharAt(i, letter);
+                        match = true;
+                    }
                 }
-            }
-            if (match) {
-                System.out.println("Letter found! Keep going.");
+                if (match) {
+                    System.out.println("Letter found! Keep going.");
+                } else {
+                    System.out.println("No matches for " + letter);
+                }
             } else {
-                System.out.println("No matches for " + letter);
+                System.out.println("Invalid input. Please enter a single letter or the complete phrase.");
             }
-        } else {
-            System.out.println("Invalid input. Please enter a single letter or the complete phrase.");
-        }
 
         } while (!phrase1.toString().contentEquals(clue));
-//        call display() in the Player class, note format obj.fx() with no arg as they're all handled in Player class
+        // call display() in the Player class, note format obj.fx() with no arg as
+        // they're all handled in Player class
         player1.display();
     }
 }
