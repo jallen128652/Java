e10e35a7fb7d17b797c2a3a0985b01146b18f223
jallen128652 <jallen128652@mymail.tstc.edu>
2023-07-20 23:13:50 -0500

    Mastery

--- Module6/practice/src/Player.java
+++ Module6/practice/src/Player.java
@@ -1,19 +1,21 @@
 public class Player {
+//    declare private obj's and vars for encapsulation
     private String name;
-    public int numAttempts;
-
+    private int numAttempts;
+//    set name from PlayerMain input
     public void setName(String playerName) {
         name = playerName;
     }
-
+//    return name to any fx that calls
+//    possibly don't need this as the obj is utilized within this class
     public String getName() {
         return name;
     }
-
+//    increment the attempts
     public void numberAttempts() {
         numAttempts++;
     }
-
+//  display results for each round
     public void display() {
         System.out.println("Player " + name + " completed the round in " + numAttempts + " attempts!");
         numAttempts = 0;
