027a055585a13addfd10ad96bfb29774686bc85f
jallen128652 <jallen128652@mymail.tstc.edu>
2023-08-08 20:39:03 -0500

    final mastery

--- Module7/mastery/src/Orders.java
+++ Module7/mastery/src/Orders.java
@@ -1,31 +1,40 @@
-import java.util.*;
 public class Orders {
 
-    private int qty, qty2, qty3;
+    private int qty1;
+    private int qty2;
+    private int qty3;
+    private double total1;
 //    set and get for items and qty's
 
     public int getHatType3(int count3){
         return count3;
     }
     public void setQty(int qty){
-        this.qty = qty;
+        this.qty1 = qty;
+        qty1 += qty;
     }
     public int getQty(){
-        return qty;
+        return qty1;
     }
     public void setQty2(int qty){
         this.qty2 = qty;
+        qty2 += qty;
     }
     public int getQty2(){
         return qty2;
     }
     public void setQty3(int qty){
         this.qty3 = qty;
+        qty3 += qty;
     }
     public int getQty3(){
         return qty3;
     }
-
-
-//    totals function
+    public void setTotal(double total){
+        this.total1 = total;
+        total1 += total;
+    }
+    public double getTotal(){
+        return total1;
+    }
 }
--- Module7/mastery/src/OrdersMain.java
+++ Module7/mastery/src/OrdersMain.java
@@ -5,6 +5,7 @@ public class OrdersMain {
 //        call fx for print menu inside do while loop first
 //        input next
 //        switch case for choice which calls fx's inside a do while loop
+        ArrayList<Orders> orders = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         int choice;
         do{
@@ -12,10 +13,10 @@ public class OrdersMain {
            choice = input.nextInt();
            switch(choice){
                case 1:
-                   orders();
+                   orders(orders);
                    break;
                case 2:
-                   salesTotals();
+                   salesTotals(orders);
                    break;
                case 3:
                    System.out.println("Exiting now.");
@@ -36,15 +37,17 @@ public class OrdersMain {
         System.out.println("Choose an option: ");
     }
 // order fx
-    public static void orders(){
+    public static void orders(ArrayList<Orders> orders){
         Orders customer = new Orders();
         int hatType = 0, qty;
+        double subTotal, tax = 0.0825, subT1, subT2, subT3;
+        double shipping = 0, total, taxes;
         double[][] hats = {{4.00, 3.75, 3.50, 3.25, 3.00},
                 {4.50, 4.25, 4.00, 3.75, 3.50},
                 {5.00, 4.75, 4.50, 4.25, 4.00}};
 
         System.out.println("Hat Prices\n");
-        System.out.println("Price by qty/type    <10    |  10-25  |  26-49  |  49-100  | >100");
+        System.out.println("Price by qty/type    <10    |  10-25  |  26-49  |  50-100  | >100");
         Scanner input = new Scanner(System.in);
         for(int a = 0; a < hats.length; ++a){
             if(a == 0){
@@ -90,7 +93,7 @@ public class OrdersMain {
 //     check the ranges and getting the priceEa
 //      use a range match
         final int NUM_RANGES = 5;
-        double[] range = {9, 25, 49, 100, 5000};
+        double[] range = {1, 10, 26, 50, 101};
         int sub1 = NUM_RANGES - 1;
         int sub2 = NUM_RANGES - 1;
         int sub3 = NUM_RANGES - 1;
@@ -112,17 +115,62 @@ public class OrdersMain {
             }
             priceEa3 = hats[2][sub3];
         }
-        System.out.println(hat1QTY + " " + priceEa1);
-        System.out.println(hat2QTY + " " + priceEa2);
-        System.out.println(hat3QTY + " " + priceEa3);
-
-//        calc subtotal
+        subT1 = priceEa1 * hat1QTY;
+        subT2 = priceEa2 * hat2QTY;
+        subT3 = priceEa3 * hat3QTY;
+        subTotal = subT1 + subT2 + subT3;
+        taxes = subTotal * tax;
+        if(subTotal < 10){
+            shipping = 5.99;
+        }else if (subTotal >= 10 && subTotal < 25){
+            shipping = 9.99;
+        } else if (subTotal >= 25 && subTotal < 50) {
+            shipping = 15.99;
+        }else if (subTotal >= 50){
+            shipping = 0;
+        }
+        total = subTotal + taxes + shipping;
+        customer.setTotal(total);
+        System.out.println("Party Hats Sales Receipt\n");
+        System.out.println(hat1QTY + " Kid's Ball Caps \t\t\t@ " + priceEa1 + " each: $ " +
+                String.format("%.2f",subT1));
+        System.out.println(hat2QTY + " Woman's Sun Hats \t\t\t@ " + priceEa2 + " each: $ " +
+                String.format("%.2f",subT2));
+        System.out.println(hat3QTY + " Men's Trucker Hats \t\t@ " + priceEa3 + " each: $ " +
+                String.format("%.2f",subT3));
+        System.out.println("Subtotal:\t\t\t$ " + String.format("%.2f",subTotal));
+        System.out.println("Tax:\t\t\t\t$ " + String.format("%.2f",taxes));
+        System.out.println("Shipping:\t\t\t$ " + String.format("%.2f",shipping));
+        System.out.println("Total Due:\t\t\t$ " + String.format("%.2f",total));
+        System.out.println("\n\n");
+//        create objects to store in the array list
+        Orders order = new Orders();
+        order.setQty(hat1QTY);
+        order.setQty2(hat2QTY);
+        order.setQty3(hat3QTY);
+        order.setTotal(total);
+        orders.add(order);
     }
 
 
 // sales total arraylist fx
-    public static void salesTotals(){
-
+    public static void salesTotals(ArrayList<Orders> orders){
+//        all 4 getters
+//        store in arraylist by customer object
+//        when fx is called print qty by type and total sales amt
+        int hat1 = 0, hat2 = 0, hat3 = 0;
+        double totals = 0;
+        for(Orders order : orders){
+            hat1 += order.getQty();
+            hat2 += order.getQty2();
+            hat3 += order.getQty3();
+            totals += order.getTotal();
+        }
+        System.out.println("Total Sales for Today\n");
+        System.out.println("Kid's Ball Caps:       " + hat1);
+        System.out.println("Woman's Sun Hats:      " + hat2);
+        System.out.println("Men's Trucker Hats:    " + hat3);
+        System.out.println("Total Amount of Sales: $" + String.format("%.2f", totals));
     }
 
 }
