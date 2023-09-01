a066177bf111a5d15714cdf02c3c7c96425f88e5
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-01 16:40:19 -0500

    commit

---
+++ AdvModule1/Practice1/src/practice1/DinnerParty.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice1;
+
+/**
+ *
+ * @author druid
+ */
+public class DinnerParty extends Party {
+    private int dinnerChoice;
+    
+    public int getDinnerChoice(){
+    return dinnerChoice;
+    }
+    public void setDinnerChoice(int choice){
+    dinnerChoice = choice;
+    }
+}
---
+++ AdvModule1/practice2/src/practice2/Party.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice1;
+
+/**
+ *
+ * @author druid
+ */
+public class Party {
+    private int guests;
+    
+    public int getGuests(){
+    return guests;
+    }
+    
+    public void setGuests(int numGuests){
+     guests = numGuests;   
+    }
+    
+    public void displayInvitation(){
+    System.out.println("Please come to my party!");
+    }
+}
