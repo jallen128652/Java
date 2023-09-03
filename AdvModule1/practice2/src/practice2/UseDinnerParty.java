f732dec8e4b4d46bea181ebe660ea4f6a6ca4172
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-03 18:39:26 -0500

    commit

---
+++ AdvModule1/practice2/src/practice2/UseDinnerParty.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice2;
+
+/**
+ *
+ * @author druid
+ */
+import java.util.*;
+public class UseDinnerParty {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int guests, choice;
+        Party aParty = new Party();
+        DinnerParty aDinnerParty = new DinnerParty();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of guests for the party >> ");
+        guests = keyboard.nextInt();
+        aParty.setGuests(guests);
+        System.out.println("The party has " + aParty.getGuests() + " guests");
+        aParty.displayInvitation();
+        System.out.println("Enter number of guests for the dinner party >> ");
+        guests = keyboard.nextInt();
+        aDinnerParty.setGuests(guests);
+        System.out.println("Enter the menu option -- 1 for chicken or 2 for beef >> ");
+        choice = keyboard.nextInt();
+        aDinnerParty.setDinnerChoice(choice);
+        System.out.println("The dinner party has " + aDinnerParty.getGuests() + " guests");
+        System.out.println("Menu option " + aDinnerParty.getDinnerChoice() + " will be served");
+        aDinnerParty.displayInvitation();
+    }
+    
+}
