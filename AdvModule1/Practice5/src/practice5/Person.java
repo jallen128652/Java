c7930d49f2d2939b04134dc7054b5a9725660f18
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-04 15:35:14 -0500

    Merge origin/master
    Conflicts:
    	AdvModule1/Practice1/src/practice1/UseParty.java

---
+++ AdvModule1/Practice5/src/practice5/Person.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice5;
+
+/**
+ *
+ * @author druid
+ */
+public class Person {
+    String name;
+    String address;
+    String phone;
+    String email;
+    
+   void purchaseParkingPass(){
+       System.out.println("Parking pass purchased");
+   } 
+    
+}
