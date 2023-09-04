ff3ca9df4777e9953a8c836739e16448500cd7d0
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-04 15:35:01 -0500

    Merge origin/master
    Conflicts:
    	AdvModule1/Practice1/src/practice1/UseParty.java

---
+++ AdvModule1/Practice5/src/practice5/Student.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice5;
+
+/**
+ *
+ * @author druid
+ */
+public class Student extends Person{
+    int idNumber;
+    float avgMark;
+    
+    boolean isEligibleToEnroll(){
+        return true;
+    }
+}
