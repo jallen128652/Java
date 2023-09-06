e8a3592bf5d98c66965ee30756be5369634cf2cf
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-05 20:35:14 -0500

    Merge origin/master
    Conflicts:
    	AdvModule1/Practice1/src/practice1/UseParty.java

--- AdvModule1/JamesAllenM1/src/PunchingParrotTravelAgency/TravelAgencyEmployee.java
+++ AdvModule1/JamesAllenM1/src/PunchingParrotTravelAgency/TravelAgencyEmployee.java
@@ -3,11 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package PunchingParrotTravelAgency;
-
 /**
  *
  * @author druid
  */
 public class TravelAgencyEmployee {
+    private int empId;
+    private String empName;
+    private String hireDate;
+    private String salary;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }  
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
     
 }
