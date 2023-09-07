753931eeb16f7f16bb79bad586696b00c2dc324d
jallen128652 <jallen128652@mymail.tstc.edu>
2023-09-07 13:15:23 -0500

    Merge origin/master
    Conflicts:
    	AdvModule1/Practice1/src/practice1/UseParty.java

--- AdvModule1/JamesAllenM1/src/PunchingParrotTravelAgency/Lodging.java
+++ AdvModule1/JamesAllenM1/src/PunchingParrotTravelAgency/Lodging.java
@@ -8,9 +8,38 @@ package PunchingParrotTravelAgency;
  *
  * @author druid
  */
-public class Lodging extends Customer{
+public class Lodging{
     String date;
-    int lodgingType;
-    double basePricePerNight;
+    String lodgingType;
     String name;
+    double basePricePerNight;
+    
+
+//   default constructor
+    Lodging(){
+//        default vals
+        date = "Default";
+        lodgingType = "Default";
+        name = "Default";
+        basePricePerNight = 150;
+        
+    }
+//    overloaded constructor
+    Lodging(String date, String lodgingType, String name, double basePricePerNight){
+//        pulls vals from person
+        this.date = date;
+        this.lodgingType = lodgingType;
+        this.name = name;
+        this.basePricePerNight = basePricePerNight;        
+    }
+//  note no inheritance so no calls to super.toString()
+    @Override
+    public String toString(){
+        return "Booking details: " +
+               "\nTravel date: " + date +
+               "\nLodging type:: " + lodgingType + 
+               "\nHotel name: " + name + 
+               "\nNightly room price: $" + String.format("%.2f", basePricePerNight) + "\n";
+    }
 }
+        
